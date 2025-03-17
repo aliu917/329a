b3f6da79563367f99f3f7c245bd42c98c6f4f5e2
Angela Liu <aliu917@berkeley.edu>
2025-03-17 13:46:10 -0700

    adds ability to change teacher during eval

--- eval.py
+++ eval.py
@@ -1,17 +1,18 @@
 import argparse
-import os
-import json
-import torch
-from torch.utils.data import DataLoader
-from tqdm import tqdm
 from project.models import StudentLMAgent
 from project.experiments import prompts
 from project import datasets
+import os
+import json
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 from project.utils import extract_text_within_box
 from project.models import verifiers
 verifier = verifiers.MATH500Verifier()
+import torch
 
-def eval(dataset, train_result_path, keys, log_dir=None, mode="test", num_examples=10, trials=1, num_context_examples=10, seed=2809):
+
+def eval(dataset, train_result_path, keys, student_model, log_dir=None, mode="test", num_examples=10, trials=1, num_context_examples=10, seed=2809):
     dataset = vars(datasets)[dataset](mode=mode)
     if not log_dir:
         log_dir = os.path.dirname(train_result_path) + "/eval"
@@ -21,7 +22,7 @@ def eval(dataset, train_result_path, keys, log_dir=None, mode="test", num_exampl
     with open(train_result_path, "r") as file:
         train_result_data = json.load(file)
     student_context_examples = prompts.get_eval_student_context(train_result_data, keys, num_context_examples)
-    student = StudentLMAgent(log_dir=log_dir)
+    student = StudentLMAgent(log_dir=log_dir, model=student_model)
     dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
 
     results = []
@@ -62,14 +63,16 @@ if __name__ == "__main__":
     parser.add_argument("--train_result_path", "-f", type=str, default=None)
     parser.add_argument("--keys", "-k", type=str, default="")
     parser.add_argument("--mode", "-m", type=str, default="test")
-    parser.add_argument("--num_eval_samples", "-n", type=int, default=10)
+    parser.add_argument("--num_eval_samples", "-n", type=int, default=20)
     parser.add_argument("--num_context_examples", "-c", type=int, default=10)
     parser.add_argument("--trials", "-t", type=int, default=1)
+    parser.add_argument("--student_model", "-s", type=str, default="Qwen/Qwen2.5-7B-Instruct-Turbo")
     args = parser.parse_args()
     eval(
         args.dataset,
         args.train_result_path,
         args.keys.split(","),
+        args.student_model,
         log_dir=args.log_dir,
         mode=args.mode,
         num_examples=args.num_eval_samples,
