201460278769ffa11f84617d6fcda79a33f7f150
Angela Liu <aliu917@berkeley.edu>
2025-03-17 18:23:16 -0700

    add checkpoint

--- eval.py
+++ eval.py
@@ -12,7 +12,7 @@ from project.models import verifiers
 verifier = verifiers.MATH500Verifier()
 
 
-def eval(dataset, train_result_path, keys, student_model, log_dir=None, mode="test", num_examples=10, trials=1, num_context_examples=10, seed=2809):
+def eval(dataset, train_result_path, keys, student_model, checkpoint_iter=0, log_dir=None, mode="test", num_examples=10, trials=1, num_context_examples=10, seed=2809):
     dataset = vars(datasets)[dataset](mode=mode)
     if not log_dir:
         log_dir = os.path.dirname(train_result_path) + "/eval"
@@ -30,7 +30,16 @@ def eval(dataset, train_result_path, keys, student_model, log_dir=None, mode="te
 
     results = []
     acc = 0
+    if checkpoint_iter > 0 and os.path.exists(log_dir + "/eval_results.json"):
+        with open(log_dir + "/eval_results.json", "r") as f:
+            results = json.load(f)
+            assert len(results) == checkpoint_iter, f"Loaded previous results of length {len(results)} but checkpoint set to {checkpoint_iter}"
+            acc = sum(1 for item in results if item.get("correct") == 1)
+            print(f"Loading from checkpoint with {acc}/{len(results)}")
+
     for i, (data, labels) in tqdm(zip(range(num_examples), dataloader)):
+        if i < checkpoint_iter:
+            continue
         x = data["problem"][0]
         y = labels[0]
         answer = str(data["answer"].item())
@@ -66,12 +75,13 @@ if __name__ == "__main__":
     parser.add_argument("--dataset", "-d", type=str, default=None)
     parser.add_argument("--log_dir", "-l", type=str, default=None)
     parser.add_argument("--train_result_path", "-f", type=str, default=None)
-    parser.add_argument("--keys", "-k", type=str, default="answer_steps")
+    parser.add_argument("--keys", "-k", type=str, default="")
     parser.add_argument("--mode", "-m", type=str, default="test")
     parser.add_argument("--num_eval_samples", "-n", type=int, default=10)
     parser.add_argument("--num_context_examples", "-c", type=int, default=10)
     parser.add_argument("--student_temp", default=0.7)
     parser.add_argument("--trials", "-t", type=int, default=1)
+    parser.add_argument("--checkpoint", "-p", type=int, default=0)
     parser.add_argument("--student_model", "-s", type=str, default="Qwen/Qwen2.5-7B-Instruct-Turbo")
     args = parser.parse_args()
     eval(
@@ -79,6 +89,7 @@ if __name__ == "__main__":
         args.train_result_path,
         args.keys.split(","),
         args.student_model,
+        args.checkpoint,
         log_dir=args.log_dir,
         mode=args.mode,
         num_examples=args.num_eval_samples,
